9fc80ae915682f73076e4faae207640789317430
frandeclich <fdeclich1@gmail.com>
2021-04-26 13:30:08 -0300

    avances

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Button from '../Button'
+import input from '../Input'
+
+const Register = (className) => {
+    return (
+        <div className={`container p-4 d-flex justify-content-center ${className}`}>
+            <div className="row justify-content-center shadow-lg p-3 mb-5 bg-body rounded">
+                <div className="col-12 col-md-9">
+                    <h3 className="text-center pb-3">Regístrate</h3>
+                </div>
+                <div className="col-12 col-md-9">
+                    <form action="" onSubmit="">
+                        <Input
+                            className="col-12"
+                            name="email"
+                            type="email"
+                            placeholder="Email"
+                        />
+                        <Input
+                            className="col-12"
+                            name="pass"
+                            type="password"
+                            placeholder="Contraseña"
+                        />
+                        <Button
+                            type='submit'
+                            text='ENVIAR'
+                        />
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/pages/SingIn.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+/*Componentes*/
+import Register from '../components/Register'
+import Login from '../components/Login'
+
+const SignIn = () => {
+    return ( 
+        <>
+        <h1 className="text-center my-4">Bienvenid@s a MovieReact perritos malvadossss</h1>
+        <div>
+            
+        </div>
+        </>
+     );
+}
+ 
+export default SignIn;
